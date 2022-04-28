dc4a6e7c937e176efbe7bec5366f340372cb00e9
OlaviLappa <tomas.shellenberg@mail.ru>
2022-04-28 08:29:19 +0300

    upload unit-tests2

--- Unit_tests/UnitTest1.cs
+++ Unit_tests/UnitTest1.cs
@@ -9,6 +9,8 @@ namespace Unit_tests
 {
     public class UnitTest1
     {
+
+        /*“естируем механизм нахождени€ площади треугольника*/
         [Fact]
         public void CalculateTriangle_Triangle_and_resultVal_return_squareFigure()
         {
@@ -30,6 +32,7 @@ namespace Unit_tests
             Assert.Equal(awaitingResult, squareTriangleResult);
         }
 
+        /*“естируем механизм нахождени€ площади круга*/
         [Fact]
         public void CalculateCircle_Circle_and_resultVal_return_squareFigure()
         {
@@ -48,6 +51,41 @@ namespace Unit_tests
 
             ///Assert
             Assert.Equal((int)awaitingResult, squareCircleResult);
+        }
+
+        /*“естируем механизм вы€влени€ пр€моугольного треугольника*/
+        [Fact]
+        public void RectangularTriangle_Triangle_return_boolValue()
+        {
+            /*“–≈”√ќЋ№Ќ»  Ќ≈ ѕ–яћќ”√ќЋ№Ќџ…*/
+            Triangle notRectangularTriangle = new Triangle()
+            {
+                SideA = 12,
+                SideB = 14,
+                SideC = 7
+            };
+
+            /*“–≈”√ќЋ№Ќ»  ѕ–яћќ”√ќЋ№Ќџ…*/
+            Triangle rectangularTriangle = new Triangle()
+            {
+                SideA = 2f,
+                SideB = 2.824f,
+                SideC = 2
+            };
+
+            Figure<Triangle> figure = new Figure<Triangle>();
+
+            bool isNotRectangularTriangle = false; //метод выдаЄт false в случае, если треугольник не пр€моугольный
+            bool isRectangularTriangle = true; //метод выдаЄт true в случае, если треугольник пр€моугольный
+
+            //ѕередаЄм не пр€моугольный треугольник, система должна выдать булевое false
+            bool awaitingResult = figure.RectangularTriangle(notRectangularTriangle);
+            Assert.Equal(awaitingResult, isNotRectangularTriangle);
+
+
+            //ѕередаЄм пр€моугольный треугольник, система должна выдать булевое true
+            bool awaitingResult2 = figure.RectangularTriangle(rectangularTriangle);
+            Assert.Equal(awaitingResult2, isRectangularTriangle);
         }
     }
 }
